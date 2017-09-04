c067746a1df7bdef32cb24d5630508d864f15ff5
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-04 20:57:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,8 +8,8 @@ function addElementToBeginningOfArray(array, element) {
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  var array = [1]
-  array.unshift("foo")
+  var array = array
+  array.unshift(element)
   return array
 }
 
