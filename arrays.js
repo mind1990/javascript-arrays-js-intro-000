8940ec41651a4736beb47a957dbc3fed6b4dcedc
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-04 20:25:13 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+
+function addElementToBeginningOfArray(array, element) {
+  var myArray = ["foo", 1]
+  myArray.unshift()
+  return myArray
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  var array = [1]
+  array = ["foo",...array]
+  return array
+}
+
+
+
+function addElementToEndOfArray(array, element) {
+  var array = [1]
+  array.push('foo')
+  return array
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  var myArray = [1]
+  myArray.push('foo')
+  return myArray
+}
